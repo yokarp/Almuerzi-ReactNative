1303179aa079b5cc86f3fe4ca91d5bd46ee4f218
Yokar <yokar@protonmail.com>
2020-10-20 04:06:27 -0500

    View login

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,60 @@
 import React from 'react';
-import { Text } from 'react-native'
+import { Text, TextInput, View, StyleSheet, Button } from 'react-native';
+import useForm from '../hooks/useForm';
 
-export default () => {
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 15
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    alignSelf: 'stretch',
+    marginBottom: 10,
+    paddingHorizontal: 5,
+  }
+})
+
+export default ({ navigation }) => {
+
+  const initialState = {
+    email: '',
+    password: '',
+  }
+
+  const onSubmit = values => {
+    console.log(values);
+  }
+
+  const { subscribe, inputs, handleSubmit } = useForm(initialState, onSubmit)
+
   return(
-    <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Iniciar Sesión</Text>
+      <TextInput
+        value={inputs.email}
+        onChangeText={ subscribe('email') }
+        style={styles.input}
+        placeholder='Email'
+      />
+      <TextInput
+        value={inputs.password}
+        onChangeText={subscribe('password')}
+        style={styles.input}
+        placeholder='Password'
+        secureTextEntry={true}
+      />
+      <Button title='Iniciar sesión' onPress={handleSubmit}/>
+      <Button title='Registrarse' onPress={() => navigation.navigate('Register') }/>
+    </View>
   )
 }
