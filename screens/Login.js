5c0eeca1f4881c55ed86ddd0dc0ba3329342bde8
Yokar <yokar@protonmail.com>
2020-10-22 14:19:09 -0500

    Login and Vercel

--- screens/Login.js
+++ screens/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TextInput, View, StyleSheet, Button } from 'react-native';
+import { Alert, Text, TextInput, View, StyleSheet, Button, AsyncStorage } from 'react-native';
 import useForm from '../hooks/useForm';
 
 const styles = StyleSheet.create({
@@ -32,7 +32,26 @@ export default ({ navigation }) => {
   }
 
   const onSubmit = values => {
-    console.log(values);
+    fetch('https://serverless.yokarp.vercel.app/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify(values),
+    })
+    .then(x => x.text())
+    .then(x => {
+      try {
+          return JSON.parse(x)
+      } catch {
+        throw x
+      }
+    })
+    .then(x => {
+      AsyncStorage.setItem('token', x.token )
+      navigation.navigate('Meals')
+    })
+    .catch(e => Alert.alert('Error', e))
   }
 
   const { subscribe, inputs, handleSubmit } = useForm(initialState, onSubmit)
@@ -41,12 +60,14 @@ export default ({ navigation }) => {
     <View style={styles.container}>
       <Text style={styles.title}>Iniciar Sesión</Text>
       <TextInput
+        autoCapitalize='none'
         value={inputs.email}
         onChangeText={ subscribe('email') }
         style={styles.input}
         placeholder='Email'
       />
       <TextInput
+        autoCapitalize='none'
         value={inputs.password}
         onChangeText={subscribe('password')}
         style={styles.input}
